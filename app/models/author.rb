2ee3ad248fb9501f846442fe91bda2dd0d745286
clairemuller <clairermuller@gmail.com>
2019-02-27 10:30:10 -0800

    all done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,42 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+
+    @@all << self
   end
 
+  # INSTANCE METHODS
+
+  def add_article(title, magazine)
+    Article.new(self, magazine, title)
+  end
+
+  def articles
+    Article.all.select do |xx|
+      xx.author == self
+    end
+  end
+
+  def magazines
+    self.articles.map do |xx|
+      xx.magazine
+    end.uniq
+  end
+
+  def show_specialties
+    self.magazines.map do |xx|
+      xx.category
+    end.uniq
+  end
+
+  # CLASS METHODS
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,36 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
 
+    @@all << self
   end
 
+  # INSTANCE METHODS
+
+  def article_titles
+    Article.all.select do |xx|
+      xx.magazine == self
+    end.map do |yy|
+      yy.title
+    end
+  end
+
+  # CLASS METHODS
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |xx|
+      xx.name == name
+    end
+
+  end
 
 end
