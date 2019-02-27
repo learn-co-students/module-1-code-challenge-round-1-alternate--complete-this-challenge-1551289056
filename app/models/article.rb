d2dd573f1997270fb94ec820821620990bbe06f5
ytsuji27 <ytsuji91@gmail.com>
2019-02-27 10:14:59 -0800

    Committing Code Challenge Solutions

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,19 @@
 class Article
 
+  @@all = []
+
+  attr_reader :author, :magazine
+  attr_accessor :title
+
+  def initialize(title, author, magazine)
+    @title = title
+    @author = author
+    @magazine = magazine
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,38 @@
 class Author
-  attr_accessor :name
 
+  attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def add_article(title, magazine)
+    Article.new(title, self, magazine)
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    self.articles.map do |article|
+      article.magazine
+    end.uniq
+  end
+
+  def show_specialties
+    self.magazines.map do |magazine|
+      magazine.category
+    end
+  end
+  
 end
