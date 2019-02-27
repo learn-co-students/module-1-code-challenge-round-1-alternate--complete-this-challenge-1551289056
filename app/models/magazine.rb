245e886c9853fdd79b202cec1f18f4e0d19d85bc
github username <github email address>
2019-02-27 10:25:53 -0800

    code challenge 1

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,37 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.magazine == self
+    end
+  end
+
+
+# Given a string of magazine's name, this method returns the first magazine object that matches
+  def self.find_by_name(name)
+    self.all.find do |magazine|
+      magazine.name == name
+    end
+  end
+
+  # Returns an array of the titles of all articles written for that magazine
+  def article_titles
+    self.articles.map do |article|
+      article.title
+    end
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,13 +6,31 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+cathy = Author.new("Cathy")
+fred = Author.new("Fred")
+seth = Author.new("Seth")
 
 
 
+magazine1 = Magazine.new("Cats Craziness", "Cat Stuff")
+magazine2 = Magazine.new("Knitting", "Knitting Stuff")
+magazine3 = Magazine.new("Climber Club", "Climbing Stuff")
 
 
+article1 = Article.new(cathy, magazine1, "How to Calm Your Feline")
+article2 = Article.new(fred, magazine2, "Getting Out Tough Knots")
+article3 = Article.new(seth, magazine3, "Rock n Roll")
+article4 = Article.new(fred, magazine3, "How to Boulder")
 
 
+# cathy.add_articles(magazine2, "How to Purl")
+#fred.articles
+#fred.magaines
+# magazine3.article_titles
+# magazine3.articles
+#Magazine.find_by_name("Climber Club")
+#article1.author
+#article1.magazine
 
 
 ### DO NOT REMOVE THIS
