a8896411b47dd85144fc51d62aa58d69039a5856
isalevine <isaacdlevine@gmail.com>
2019-02-27 10:11:25 -0800

    code challenge complete, all tests passing

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,31 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def articles
+    Article.all.select { |article| article.author == self }
+  end
+
+  def magazines
+    self.articles.map { |article| article.magazine }.uniq
+  end
+
+  def add_article(title, magazine)
+    Article.new(self, magazine, title)
+  end
+
+  def show_specialties
+    self.magazines.map { |magazine| magazine.category }
+  end
 
 end
