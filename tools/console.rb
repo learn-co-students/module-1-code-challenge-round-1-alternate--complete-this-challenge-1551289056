2228d75af0be40f280a1addf8ff824bdd9c9d854
Joshua Loran <joshua.loran@gmail.com>
2019-02-27 10:32:07 -0800

    challenge1 95%

--- tools/console.rb
+++ tools/console.rb
@@ -7,11 +7,22 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+ted = Author.new("TED")
+bill = Author.new("Bill")
+jill = Author.new("Jill")
+jen = Author.new("Jen")
+
+magazine1 = Magazine.new("Mag1", "Sports")
+magazine2 = Magazine.new("Mag2", "Beauty")
+magazine3 = Magazine.new("Mag3", "Coooking")
+magazine4 = Magazine.new("Mag4", "Driving")
 
 
+article1 = Article.new(ted, magazine1)
+article2 = Article.new(bill, magazine2)
 
-
-
+ted.add_article("baking", magazine3)
+jill.add_article("driving", magazine4)
 
 
 
