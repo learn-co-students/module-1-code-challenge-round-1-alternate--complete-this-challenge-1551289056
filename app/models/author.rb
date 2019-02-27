746e8671eadb738eea2db6648bccfa2f5a7f4345
github username <github email address>
2019-02-27 10:16:14 -0800

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,38 @@
 class Author
   attr_accessor :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+
+    @@all << self
   end
 
+  def add_article(title, magazine)
+    Article.new(magazine, self, title)
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    self.articles.map do |article|
+      article.magazine
+    end
+  end
+
+  def show_specialties
+    self.magazines.map do |magazine|
+      magazine.category
+    end
+  end
+
+  def self.all
+    @@all
+  end
 
 end
