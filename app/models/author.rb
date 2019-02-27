09234c0476e053f79c8e7b11c0a30ebc6033eee9
Kimberly Heidorn <heidorkn@gmail.com>
2019-02-27 10:22:01 -0800

    Kim Code Challenge 1 Complete

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,40 @@
 class Author
-  attr_accessor :name
-
+  attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def add_article(title, magazine)
+    Article.new(title, self, magazine)
+  end
+
+  def articles
+    Article.all.select do |instance|
+      instance.author == self
+    end
+  end
+
+  def magazines
+    author_magazines = []
+    self.articles.each do |instance|
+      author_magazines << instance.magazine
+    end
+    author_magazines
+  end
+
+  def show_specialties
+    categories = []
+    self.magazines.each do |instance|
+      categories << instance.category
+    end
+    categories.uniq
+  end
+  
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,26 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+#Authors
+nich = Author.new("Nich")
+kim = Author.new("Kim")
+rafa = Author.new("Rafa")
+ruth = Author.new("Ruth")
 
+#Magazines
+vanity_fair = Magazine.new("Vanity Fair", "Fashion")
+cosmo = Magazine.new("Cosmopolitan", "Fashion")
+sports = Magazine.new("Sports Illustrated", "Sports")
+new_yorker = Magazine.new("The New Yorker", "News")
+
+#Articles
+Article.new("What Men Think", nich, cosmo)
+Article.new("Seahawks Trading QB?", nich, sports)
+Article.new("Men's Fashion", nich, vanity_fair)
+Article.new("The Green New Deal", kim, new_yorker)
+Article.new("What Women Think", kim, cosmo)
+Article.new("What Dogs Think", rafa, cosmo)
+Article.new("The Future of E-Sports", ruth, sports)
 
 
 
