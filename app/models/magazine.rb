c29ab39bdbe29c925061ec4da34c7746694acad0
kb2shy <chhaianpin@hotmail.com>
2019-02-27 10:17:56 -0800

    Done.

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,28 @@
 class Magazine
-  attr_accessor :name, :category
 
+  @@all = []
+  attr_accessor :name, :category
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def article_titles
+    Article.all.select do |article|
+      article.magazine == self
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |magazine|
+      magazine.name == name
+    end
+  end
 
 end
