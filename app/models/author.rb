92f775f58d77049a4b8090f6158aa09a154f1875
github username <github email address>
2019-02-27 10:26:41 -0800

    Done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,36 @@
 class Author
-  attr_accessor :name
-
+  attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def add_article(title, magazine)
+    Article.new(self, magazine, title)
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    articles.map do |article|
+      article.magazine
+    end
+  end
+
+  def show_specialties
+    magazines.map do |magazine|
+      magazine.category
+    end
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,7 +6,17 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+matt = Author.new("Matt")
+john = Author.new("John")
 
+m1 = Magazine.new("M1", "Horror")
+m2 = Magazine.new("M2", "Adventure")
+m3 = Magazine.new("M3","Action")
+
+a1 = Article.new(matt, m1, "title1")
+a2 = Article.new(matt, m2, "title2")
+a3 = Article.new(matt, m3, "title3")
+a4 = Article.new(john, m1, "title4")
 
 
 
